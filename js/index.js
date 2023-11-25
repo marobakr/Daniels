f0ae908d75270b63eaac05c7ad0f2c1fa24a8563
marwanabubakr <dev@marobakr>
2023-11-25 11:24:09 +0200

    hello world Done

--- js/index.js
+++ js/index.js
@@ -1,16 +1,57 @@
-// change Position & bg for Nav
+// change bg and text color for links
+document.addEventListener('DOMContentLoaded', function () {
+  var links = document.querySelectorAll('a')
+  window.addEventListener('scroll', function () {
+    var scrollPosition = window.scrollY
+    var scrollThreshold = 200
+
+    links.forEach(function (link) {
+      if (scrollPosition > scrollThreshold) {
+        link.classList.add('text-black', 'psoudo-black')
+        link.classList.remove('text-white', 'psoudo-white')
+      } else {
+        link.classList.add('text-white', 'psoudo-white')
+        link.classList.remove('text-black', 'psoudo-black')
+      }
+    })
+  })
+})
+// change bg nav scrolling
 window.addEventListener('scroll', () => {
   const verticalScrollPx = window.scrollY || window.pageYOffset
   let navBar = document.querySelector('nav')
-  if (verticalScrollPx < 300) {
-    navBar.classList.add('not-scrolling')
-    navBar.classList.remove('scrolling')
-  } else if (verticalScrollPx > 300 && verticalScrollPx < 1000) {
-    navBar.classList.add('scrolling')
-    navBar.classList.remove('not-scrolling')
+  if (verticalScrollPx < 200) {
+    navBar.classList.remove('bg-white', 'shadow')
+  } else if (verticalScrollPx > 200 && verticalScrollPx < 1000) {
+    navBar.classList.add('bg-white', 'shadow')
   }
 })
+// Select Skills Selector
+let ourSkills = document.querySelector('.skills')
 
+  window.onscroll = function () {
+  //Skills Offset Top
+  let skillsOffsetTop = ourSkills.offsetTop;
+
+  //Skills Outer Height
+  let skillsOuterHeight = ourSkills.offsetHeight;
+  // Window Height
+  let windowHeight = this.innerHeight;
+
+  //window ScrollTop
+  let windowScrollTop = window.scrollY
+
+  if (windowScrollTop > skillsOffsetTop + skillsOuterHeight - windowHeight) {
+
+    let allSkills = document.querySelectorAll(
+      '.progresspars .progress .progress-bar '
+    )
+    allSkills.forEach((skill) => {
+      skill.style.width = skill.dataset.progress;
+    });
+  }
+
+}
 // function Get Year of footer
 const currentDate = new Date().getFullYear()
 const yearElement = (document.getElementById(
@@ -22,8 +63,6 @@ const button = document.querySelector('.scrollToTopBtn')
 
 const displayButton = () => {
   window.addEventListener('scroll', () => {
-    console.log(window.scrollY)
-
     if (window.scrollY > 100) {
       button.style.display = 'block'
     } else {
